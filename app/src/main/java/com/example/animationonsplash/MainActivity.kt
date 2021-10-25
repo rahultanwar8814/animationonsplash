23495677814c7fe3c624388ee251ea6cb32b35d5
rahul <hulkvsironmanmark2@gamil.com>
2021-10-25 17:16:59 +0530

    animations

---
+++ app/src/main/java/com/example/animationonsplash/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.animationonsplash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.Button
+
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.fab)
+
+        button.setOnClickListener(){
+            startActivity(Intent(this@MainActivity, NextActivity::class.java))
+
+
+        }
+
+    }
+}
